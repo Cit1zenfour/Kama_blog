4cb74821c16ba611a5e26885cfe447ef9021208e
Kama <westcoast9810@gmail.com>
2020-06-10 01:19:36 +0500

    Third attempt

--- firstBlog/settings.py
+++ firstBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'egjmz6+i)6_cwrma77!(d)17)p7dr2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
